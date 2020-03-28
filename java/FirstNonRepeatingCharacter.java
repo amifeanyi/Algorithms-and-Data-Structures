7f4e027a297bb4ee20703b605b2389f66b6bdc57
ifeanyi <ifeanyiigili@gmail.com>
2020-03-28 21:12:37 +0100

    optimiized FirstNonRepeatingCharacter scala version

--- java/FirstNonRepeatingCharacter.java
+++ java/FirstNonRepeatingCharacter.java
@@ -26,7 +26,7 @@ public class FirstNonRepeatingCharacter{
 
 
     public static void main(String[] args) {
-        String data = "abc";
+        String data = "ababc";
         System.out.println(findFirstNonRepeatingCharater(data));
     }
 }
--- scala/FirstNonRepeatingCharacter.scala
+++ scala/FirstNonRepeatingCharacter.scala
@@ -5,15 +5,15 @@ class FirstNonRepeatingCharacter{
 	def findFirstNonRepeatingCharacter(text: String) = {
 		val dataSet = LinkedHashMap.empty[Char, Int]
 		
-		for(c <- text){
+		for(c <- text)
 			if(dataSet.contains(c))
-				dataSet(c) = dataSet(c) + 1
+				dataSet(c) +=  1
 			else
 				dataSet(c) = 0
-		}
 		
-		val res = for((k, v) <- dataSet; n = k; if (v == 0) )	 
-		yield n
+		
+		val res = for((k, v) <- dataSet; if (v == 0) )	 
+		yield k
 		
 		if (!res.isEmpty) res.head
 	}
@@ -28,6 +28,6 @@ object FirstNonRepeatingCharacter {
 		if(args.length != 0)
 			println(fnrChar.findFirstNonRepeatingCharacter(args(0)))
 		else	
-			println(fnrChar.findFirstNonRepeatingCharacter("ababc"))
+			println(fnrChar.findFirstNonRepeatingCharacter("ababccdsd"))
 	}
 }
